6cf30dec9b76c1b1ce8ff360154229c3a981388e
paulmorar <paulcornelmorar@gmail.com>
2019-11-26 10:57:59 +0100

    Update layout component

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -6,12 +6,16 @@
  */
 
 import React from "react"
-import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
 
 import "./layout.css"
 
-const Layout = ({ children, style }) => (
+interface Props {
+  children: Node
+  style?: object
+}
+
+const Layout = ({ children, style = {} }: Props) => (
   <StaticQuery
     query={graphql`
       query SiteTitleQuery {
@@ -29,9 +33,5 @@ const Layout = ({ children, style }) => (
     )}
   />
 )
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
 
 export default Layout
